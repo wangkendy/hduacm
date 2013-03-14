d1465d7edb769fce43caaea03bb1d658c371b2e0
wangkendy <wkendy@gmail.com>
2013-03-14 16:15:07 +0800

    A + B

---
+++ 1000.cpp
@@ -0,0 +1,19 @@
+/*
+ * Source: http://acm.hdu.edu.cn/showproblem.php?pid=1000
+ * Description: A + B
+ * tag: 
+ * author: wangkendy (wkendy@gmail.com)
+ *
+ */
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b;
+    while (cin >> a >> b) {
+        cout << a + b << endl;
+    }
+    return 0;
+}
